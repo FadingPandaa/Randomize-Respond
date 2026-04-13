f62d980fa007c523420ce67a560e20dd10ac3218
Keabetswe Masole <knmmasole@gmail.com>
2026-04-13 12:17:46 +0200

    Codes
    Codes uploaded and ready.

---
+++ Randomize Respond/randomize_respond/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomize_respond
+{   //Main method
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new randomization(){ };
+
+        }
+    }
+}
---
+++ Randomize Respond/randomize_respond/randomization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace randomize_respond
+{
+    //Start 
+   public class randomization
+    {
+        public randomization()
+        {
+            //Storing information for randomization
+
+            List<string> respond = new List<string>(){"num1","num2","num3","num4","num5"};//paralle arrary
+            //create a instanes for random class
+            Random find_index = new Random();
+
+
+            //get a randomize index
+            int random_index = find_index.Next(0,respond.Count);
+
+            //Display a randomize index
+            Console.WriteLine(respond[random_index]);
+
+
+
+
+
+
+
+
+
+
+
+        }//end constructor
+    }//end of class
+}// end of namespace
